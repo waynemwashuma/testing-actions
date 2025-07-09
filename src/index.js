2b3ea0058efe1c06216797d6ed57f52f567028c5
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-07-09 15:12:38 +0300

    fix issue

--- src/index.js
+++ src/index.js
@@ -9,4 +9,8 @@ export function add(a,b) {return a + b}
  */
 export function sub(a,b) {return a - b}
 
+/**
+ * @param {number} a 
+ * @param {number} b 
+ */
 export function multiply(a,b) {return a - b}
